443824aad053a9b127ea5232d9c4240014965e3b
parasquid <tristan.gomez@gmail.com>
2015-08-28 02:08:33 +0800

    add redirect to referral page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    redirect_to referrals_path
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,12 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  let(:user) { build_stubbed(User) }
+  let(:test_email) { 'test@example.com'}
   describe "GET new" do
-    let(:user) { build_stubbed(User) }
     it "has a 200 status code" do
       get :new
       expect(response.status).to eq(200)
+    end
+
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template("new")
+    end
+  end
+
+  describe "POST create" do
+    it "redirects to referral page" do
+      post :create, email: test_email
+      expect(response).to redirect_to(referrals_path)
+    end
+
+    pending context "new user" do
+      it "creates a new account" do
+        allow_any_instance_of(User).to receive(:save).and_return(true)
+        post :create, email: test_email
+        expect(User).to receive(:save)
+      end
     end
   end
 end
