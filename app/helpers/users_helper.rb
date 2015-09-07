ea39f97e4031b02756812436a3c3925fb60396df
parasquid <tristan.gomez@gmail.com>
2015-09-08 01:52:55 +0800

    add fb link sharer

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,19 @@
 module UsersHelper
+
+  TWITTER_SHARE_URL = 'http://twitter.com/share'
+  FACEBOOK_SHARE_URL = 'http://www.facebook.com/sharer/sharer.php'
+
   def tweet_link(referral_code, message)
-    link_to("http://twitter.com/share?url=#{CGI::escape root_url}&ref=#{CGI::escape referral_code}&text=#{CGI::escape message}".html_safe, class: 'twit', target: '_blank') {}
+    url = CGI::escape root_url
+    ref = CGI::escape referral_code
+    text = CGI::escape message
+    link_to("#{TWITTER_SHARE_URL}?url=#{url}&ref=#{ref}&text=#{text}".html_safe, class: 'twit', target: '_blank') {}
+  end
+
+  def fb_link(referral_code, message)
+    url = CGI::escape root_url
+    ref = CGI::escape referral_code
+    text = CGI::escape message
+    link_to("#{FACEBOOK_SHARE_URL}?u=#{url}&ref=#{ref}&title=#{text}".html_safe, class: 'fb', target: '_blank') {}
   end
 end
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -1,11 +1,23 @@
 require "rails_helper"
 
 RSpec.describe UsersHelper, :type => :helper do
+  let(:host) { CGI::escape 'http://test.host/' }
+  let(:referral_code) { 'abcd1234' }
+  let(:message) { 'message' }
+
   describe "#tweet_link" do
-    let(:referral_code) { 'abcd1234' }
-    let(:message) { 'tweet message' }
-    it "returns a twitter link with the referral code" do
-      expect(helper.tweet_link(referral_code, message)).to eq('<a class="twit" target="_blank" href="http://twitter.com/share?url=http%3A%2F%2Ftest.host%2F&ref=abcd1234&text=tweet+message"></a>')
+    let(:share_url) { 'http://twitter.com/share' }
+    it "returns a twitter link" do
+      href = "#{share_url}?url=#{host}&ref=#{referral_code}&text=#{message}"
+      expect(helper.tweet_link(referral_code, message)).to eq("<a class=\"twit\" target=\"_blank\" href=\"#{href}\"></a>")
+    end
+  end
+
+  describe "#fb_link" do
+    let(:share_url) { 'http://www.facebook.com/sharer/sharer.php' }
+    it "returns an fb link" do
+      href = "#{share_url}?u=#{host}&ref=#{referral_code}&title=#{message}"
+      expect(helper.fb_link(referral_code, message)).to eq("<a class=\"fb\" target=\"_blank\" href=\"#{href}\"></a>")
     end
   end
 end
